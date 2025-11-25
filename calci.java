4996a764d962ed3872c1891efc732f9fd04b3739
Anandh-2809 <anandharaj28032005@gmail.com>
2025-11-25 12:33:27 +0530

    version1

---
+++ calci.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class calci{
+public static void main(String[] args){
+    int a=10;
+    int b=20;
+    System.out.println("add "+(a+b));
+    System.out.println("sub "+(b-a));
+    
+}
+}
