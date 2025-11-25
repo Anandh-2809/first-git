7c51bc34e29c3b0a38409b3dda2cbe296deafce9
Anandh-2809 <anandharaj28032005@gmail.com>
2025-11-25 13:18:08 +0530

    10/5

--- div.java
+++ div.java
@@ -1,9 +1,13 @@
 public class div {
     public static void main(String[] args) {
 
-          int a=10;
+      int a=10;
        int b=20;
-       System.out.println("div "+b/a);
+       int c=5;
+       int d=2;
+       System.out.println("div "+a/c);
+       
+       
 
     }
     
